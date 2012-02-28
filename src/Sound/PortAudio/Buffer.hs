2cc404b9b7e79e64546628aff75480e6a501bb72
a-wild-tigger <a.wild.tigger@gmail.com>
2012-02-28 09:24:24 -0600

    added documentation

--- src/Sound/PortAudio/Buffer.hs
+++ src/Sound/PortAudio/Buffer.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+-- | This module provides the 'Buffer' type class that abstracts the array type that is being used for I\/O. Inspired by hsndfile.
 
 module Sound.PortAudio.Buffer where
 
@@ -7,23 +8,31 @@ import qualified Sound.PortAudio.Base as Base
 import Foreign.C.Types (CULong)
 import Foreign.ForeignPtr (ForeignPtr, newForeignPtr_)
 
--- The Int item is the Length, we always assume offset is 0
+
+
+
+-- | Buffer class for I\/O on PortAudio Buffers.
 class Buffer a e where
+    -- | Construct a buffer from a 'ForeignPtr' and the element count.
     fromForeignPtr :: ForeignPtr e -> Int -> IO (a e)
+    
+    -- | Retrieve from a buffer a 'ForeignPtr' pointing to its data and an element count.
     toForeignPtr   :: a e -> IO (ForeignPtr e, Int)
 
--- We need to associate stream info, since we need channel sizes
+-- | Type of callbacks that will be called with input samples that should provide output samples.
+-- This can work with arbitrary user specified buffers, as opposed to raw C arrays (ForeignPtr).
+-- See @StreamCallback@ for the more information.
 type BuffStreamCallback input output a b =
-        Base.PaStreamCallbackTimeInfo
-    ->  [StreamCallbackFlag]
-    ->  CULong
-    ->  a input
-    ->  b output
-    ->  IO StreamResult
+        Base.PaStreamCallbackTimeInfo    -- ^ Timing information for the input and output
+    ->  [StreamCallbackFlag]             -- ^ Status flags
+    ->  CULong                           -- ^ # of input samples
+    ->  a input                          -- ^ input samples
+    ->  b output                         -- ^ where to write output samples
+    ->  IO StreamResult                  -- ^ What to do with the stream, plus the output to stream
 
 
--- In general if you have decided to use vectors underneath, you cannot freeze them, as that will
--- mess things up since they are simply pointers underneath
+
+-- | Wrap a "cooked" buffer callback into the generic stream callback type.
 buffCBtoRawCB :: (StreamFormat input, StreamFormat output, Buffer a input, Buffer b output) =>
     BuffStreamCallback input output a b -> Stream input output -> StreamCallback input output    
 buffCBtoRawCB func strm = \a b c d e -> do
@@ -34,13 +43,17 @@ buffCBtoRawCB func strm = \a b c d e -> do
     func a b c storeInp storeOut
     
 
--- We assume the Buffer is long enough!!!
+-- | This Function is a high level version of @readStream@ which does not deal with Pointers
+-- and can operate on any type which is an instance of @Buffer@, possible a storable vector. It is assumed that
+-- the buffer is correctly sized, its length must be the number of frames times the channels in the underlying output stream.
 readBufferStream :: (Buffer a input, StreamFormat input, StreamFormat output) => Stream input output -> CULong -> a input -> IO (Maybe Error)
 readBufferStream a b c = do
     (c', _) <- toForeignPtr c
     readStream a b c'
 
-
+-- | This Function is a high level version of @writeStream@ which does not deal with Pointers
+-- and can operate on any type which is an instance of @Buffer@, possible a storable vector. It is assumed that
+-- the buffer is correctly sized, its length must be the number of frames times the channels in the underlying output stream.
 writeBufferStream :: (Buffer a output, StreamFormat input, StreamFormat output) => Stream input output -> CULong -> a output -> IO (Maybe Error)
 writeBufferStream a b c = do
     (c', _) <- toForeignPtr c
